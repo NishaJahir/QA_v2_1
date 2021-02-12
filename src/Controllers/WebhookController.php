86b1e9ed96a5db609dfd1fa36ea40890ebfcf581
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2021-02-12 19:47:45 +0530

    Update WebhookController.php

--- src/Controllers/WebhookController.php
+++ src/Controllers/WebhookController.php
@@ -571,11 +571,13 @@ class WebhookController extends Controller
 	
 	public function handleNnCredit()
     {
+		$this->getLogger(__METHOD__)->error('type', $this->eventType);
 		$webhookMessage  = sprintf($this->paymentHelper->getTranslatedText('callbackInitialExecution', $this->orderLanguage), $this->parentTid, ($this->eventData['transaction']['amount'] / 100), $this->eventData['transaction']['currency'], date('Y-m-d H:i:s'), $this->eventTid ).'</br>';
 		if ($this->eventType == 'ONLINE_TRANSFER_CREDIT') {
 			$webhookMessage .= sprintf($this->paymentHelper->getTranslatedText('callback_status_change',$this->orderLanguage), (float) ($this->eventData['transaction']['amount'] / 100), $this->transactionHistory->orderNo );
 			$this->paymentCreation($webhookMessage);
 		} elseif(in_array($this->eventType, ['INVOICE_CREDIT'])) {
+			$this->getLogger(__METHOD__)->error('credtr', $this->transactionHistory);
 				if ($this->transactionHistory->orderPaidAmount < $this->transactionHistory->orderTotalAmount) {
                     $this->saveTransactionLog($this->transactionHistory);
 					$this->paymentCreation($webhookMessage);
@@ -630,6 +632,7 @@ class WebhookController extends Controller
    public function paymentCreation($message, $partialRefund = false)
     {
 	   $transactionDetails = $this->paymentService->getDatabaseValues($this->transactionHistory->orderNo);
+	   $this->getLogger(__METHOD__)->error('DDD', $transactionDetails);
 	   $requestData['mop']         = $this->transactionHistory->mopId;
            $requestData['booking_text']  = $message;
 	   if(empty($this->eventData['transaction']['currency'])) {

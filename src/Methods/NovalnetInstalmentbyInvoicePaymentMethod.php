b9cf57c1371d43f22bcdab26769384dad089e9a7
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2021-02-15 11:18:50 +0530

    Update NovalnetInstalmentbyInvoicePaymentMethod.php

--- src/Methods/NovalnetInstalmentbyInvoicePaymentMethod.php
+++ src/Methods/NovalnetInstalmentbyInvoicePaymentMethod.php
@@ -84,11 +84,10 @@ class NovalnetInstalmentbyInvoicePaymentMethod extends PaymentMethodBaseService
             if (!empty($minimumAmount) && is_numeric($minimumAmount) && $minimumAmount < 1998) {
                 $instalmentPaymentMimimumAmount = false;
             }
-            $paymentConditionValidation = true;
-            if (!empty($minimumAmount)) {
+          
             $paymentConditionValidation = $this->paymentService->checkPaymentDisplayConditions($this->basket, 'novalnet_instalment_invoice');
-            }
-            $this->getLogger(__METHOD__)->error('called instalment', $paymentConditionValidation);
+            
+            $this->getLogger(__METHOD__)->error('updated instalment', $paymentConditionValidation);
             return (bool)($this->paymentService->isPaymentActive($this->basket, 'novalnet_instalment_invoice') && $instalmentPaymentMimimumAmount && $paymentConditionValidation);
         }
         return false;
